816a7f17b256c12221f0c7300bf933f91ef31cce
Michael Villafuerte <mvillafu@mymail.mines.edu>
2017-09-11 15:46:52 -0600

    Right now i'm trying to create a card class so that we can just load a deck array full of these clases. The vaialbes are symbol (A, J, K..) and suit ( SPADE, CLUB, ..).

--- War-and-Trash/main.cpp
+++ War-and-Trash/main.cpp
@@ -1,3 +1,29 @@
+// Our included files
+
+
+// The card class
+class Card {
+public:
+  enum Suit {
+    SPADE, CLUB, DIAMOND, HEART
+  };
+
+  Card(char symbol, enum suit) { 
+    this.symbol = symbol; 
+    this.suit = suit;
+  }
+
+private:
+  Card::char symbol; // This will be between
+  Card::enum suit;
+};
+
+
+// this will initialize the deck with 
+void initialize() { 
+
+}
+
 int main() {
 
 
